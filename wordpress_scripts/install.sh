e540214497b8ce0482760a48b76fffc538558b73
Maximo Lopez <7888441+mxlopez25@users.noreply.github.com>
2020-08-09 11:52:05 -0400

    Update on wordpress installation script

--- wordpress_scripts/install.sh
+++ wordpress_scripts/install.sh
@@ -2,7 +2,7 @@ echo '========================== WORDPRESS INSTALL START =======================
 
 cd /tmp &&
 curl -O https://wordpress.org/latest.tar.gz &&
-tar xzvf latest.tar.gz &&
+tar xzvf latest.tar.gz --overwrite &&
 touch /tmp/wordpress/.htaccess &&
 chmod 660 /tmp/wordpress/.htaccess &&
 cp /tmp/wordpress/wp-config-sample.php /tmp/wordpress/wp-config.php &&
@@ -13,9 +13,11 @@ sudo find /var/www/html -type d -exec chmod g+s {} \; &&
 sudo chmod g+w /var/www/html/wp-content &&
 sudo chmod -R g+w /var/www/html/wp-content/themes &&
 sudo chmod -R g+w /var/www/html/wp-content/plugins &&
+sudo chmod -R 777 /var/www/html &&
 rm /var/www/html/index.html &&
 sudo sed -i 's/define( '\''DB_NAME'\'', '\''database_name_here'\'' );/define( '\''DB_NAME'\'', '\''wordpress'\'' );/g' /var/www/html/wp-config.php &&
 sudo sed -i 's/define( '\''DB_USER'\'', '\''username_here'\'' );/define('\''DB_USER'\'', '\''wordpressuser'\'');/g' /var/www/html/wp-config.php &&
 sudo sed -i 's/define( '\''DB_PASSWORD'\'', '\''password_here'\'' );/define('\''DB_PASSWORD'\'', '\''password'\'');/g' /var/www/html/wp-config.php &&
+sudo echo 'define('\''FS_METHOD'\'', '\''direct'\'');' >> /var/www/html/wp-config.php &&
 
 echo '========================== WORDPRESS INSTALL DONE ==========================';
